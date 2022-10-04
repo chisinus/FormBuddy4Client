ec7ce8314c3824654f9f681aaa610963fa2e3af3
chisinus <chisinus@gmail.com>
2022-10-04 01:08:06 -0400

    finish login-ui

--- src/app/modules/registration/registration-routing.module.ts
+++ src/app/modules/registration/registration-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegBasicInfoComponent } from './reg-basic-info/reg-basic-info.component';
+import { RegConfirmationComponent } from './reg-confirmation/reg-confirmation.component';
+import { RegLoginComponent } from './reg-login/reg-login.component';
+import { RegSecurityQuestionComponent } from './reg-security-question/reg-security-question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'reg-basc',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'reg-basic',
+  //   component: RegBasicInfoComponent,
+  // },
+  // {
+  //   path: 'reg-sq',
+  //   component: RegSecurityQuestionComponent,
+  // },
+  // {
+  //   path: 'reg-login',
+  //   component: RegLoginComponent,
+  // },
+  // {
+  //   path: 'reg-confirm',
+  //   component: RegConfirmationComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RegistrationRoutingModule { }
+export class RegistrationRoutingModule {}
